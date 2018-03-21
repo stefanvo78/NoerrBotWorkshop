4bb1c3cb02078508a4c5d98aece8571d04fd651e
Stefan Volkmer <s.volkmer@orange-networks.de>
2018-03-21 10:44:28 +0100

    Version 2

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                await context.PostAsync($"{this.count++}: You said {message.Text} Bot Version 2");
                 context.Wait(MessageReceivedAsync);
             }
         }
